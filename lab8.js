797fe0d02a95073305c371ab07a4ab924cfb17e5
Трубицына Ершова <ershovaclara@yandex.ru>
2023-05-27 22:05:53 +0700

    Попытка

--- lab8.js
+++ lab8.js
@@ -8,3 +8,14 @@ function showDate(){
     'Дата и время для локали Франции: '+today.toLocaleString('fr-FR')+"<br />"+
     'Дата и время для локали Италии: '+today.toLocaleString('it-IT');/*Запись даты в тег*/
 }
+
+function showDaysCount(){
+    let days=document.getElementById('days'); 
+    let today = new Date(); /*Определяем текующую дату*/
+    let inputDate = document.querySelector('input[type=date]');/*Поиск тега с датой*/
+    let birthday = new Date(inputDate.value);/*Берем значение*/
+    let daysCount = (today-birthday); /*Высчитываем дни*/
+    daysCount = Math.floor(daysCount)/1000/60/60/24;/*Округляем*/
+    daysCount.innerHTML=('Количество дней с даты рождения: '+daysCount);
+    days.appendChild(daysCount);
+}
